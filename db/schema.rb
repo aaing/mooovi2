8142e8c06bf96090ac86afbc443f7c812ba6f262
aaing <n.y1031@gmail.com>
2018-02-19 17:12:49 +0900

    add index to product for search

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170222104953) do
+ActiveRecord::Schema.define(version: 20180209091044) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20170222104953) do
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
   end
+
+  add_index "products", ["title"], name: "index_products_on_title", using: :btree
 
   create_table "reviews", force: :cascade do |t|
     t.integer  "rate",       limit: 4
